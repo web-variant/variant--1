c894806811af318bc87816623162a9c9af7a1c5b
Rump-Force <super.cincinnati@yandex.com>
2026-02-26 01:08:25 +0300

    web var-1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+	loop: true,
+	// If we need pagination
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+})
